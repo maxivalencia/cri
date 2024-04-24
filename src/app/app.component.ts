d1cba98119f77a725c635a1372eafead5b1173ef
kaira <husaynmozart@gmail.com>
2024-04-24 09:37:26 +0300

    avancement controle inopiné

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import anomalies from "../../assets/data/anomalies.json";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  title = 'json-file-read-angular';
+  //public anomaliesList:{name:string}[] = anomalies;
   constructor() {}
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+//import { Http } from '@angular/http';
+//import 'rxjs/add/operator/map';
+//import 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +14,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  anomalies: any;
+  papiers: any;
+  // adresse: string = "http://192.168.12.251:2057/liste/";
+  adresse: string = "http://192.168.12.251:2057/liste/";
 
+  constructor(public http: HttpClient) {
+    console.log('Chargement des constructeur');
+    this.getAddress().subscribe(adresse => {
+      console.log(adresse.text());
+      this.adresse = adresse.text();
+    })
+    this.getAnomaliesJSON().subscribe(anomalies => {
+      console.log(anomalies);
+      this.anomalies = anomalies;
+    });
+    this.getPapiersJSON().subscribe(papiers => {
+      console.log(papiers);
+      this.papiers = papiers;
+    });
+  }
+
+
+  ngOnIt() {
+    console.log('Fin chargement data');
+  }
+
+  public getAnomaliesJSON(): Observable<any> {
+    // return this.http.get("./assets/data/anomalies.json");
+    return this.http.get(this.adresse + "anomalies");
+  }
+
+  public getPapiersJSON(): Observable<any> {
+    // return this.http.get("./assets/data/papiers.json");
+    return this.http.get(this.adresse + "papiers");
+  }
+
+  public getAddress(): Observable<any> {
+    return this.http.get("./assets/data/adresse.txt");
+  }
 }
