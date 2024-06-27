6c94f978d8827e5ff43a4e5a3a2e3dcf67cdedea
kaira <husaynmozart@gmail.com>
2024-06-27 09:45:35 +0300

    avancement application

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -6,6 +6,10 @@ import{ GlobalData } from '../global_data';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@ionic/storage-angular';
 import { App } from '@capacitor/app';
+import { AlertController } from '@ionic/angular';
+import { Network as CapacitorNetwork } from '@capacitor/network';
+import { Plugins } from '@capacitor/core';
+const { Network } = Plugins;
 
 @Component({
   selector: 'app-login',
@@ -25,48 +29,72 @@ export class LoginPage implements OnInit {
   id_user : any;
   access_level : any;
   show_menu = false;
+  public_login_store : string = "login";
+  status : boolean = false;
+
+  isAlertOpen = false;
+  alertButtons = ['Action'];
 
   constructor(public http: HttpClient,
     private router: Router,
     public globalData: GlobalData,
-    private storage: Storage
+    private storage: Storage,
+    private alertController: AlertController
   ) {
     if(this.globalData.getIdUser() > 0 && this.globalData.getUserAccessLevel() <= 4){
       this.router.navigate(['/home']);
     }
   }
 
-  ngOnInit() {
+  setOpen(isOpen: boolean) {
+    this.isAlertOpen = isOpen;
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alerte connection',
+      subHeader: "Vous n'avez pas de connexion internet",
+      message: this.message_connection,
+      buttons: ['Ok'],
+    });
+
+    await alert.present();
+  }
+
+  async ngOnInit() {
     console.log('Initiation page login');
     //this.getLoginToFile();
-    this.storage.create();
+    await this.storage.create();
     this.retrieveData();
+    //await this.checkInternetConnection()
   }
 
   signIn() {
     //console.log("En attente du service d'authentification ...!")
     //var _this = this;
-    this.getAddress().subscribe((adresse : any) => {
-      this.adresse = adresse.trim();
-      this.getAuthentification().subscribe(auth => {
-        console.log(auth);
-        this.auth = auth;
-        this.message_connection = auth["message"];
-        if(auth["code"] == 200){
-          this.id_user = auth["id_user"];
-          this.access_level = auth["access_level"];
-          this.globalData.setIdUser(this.access_level);
-          this.globalData.setIpAddress(this.adresse);
-          this.globalData.setUserAccessLevel(this.access_level);
-          //this.router.navigate(['/login']);
-          this.router.navigate(['/home']);
-        }
-        //this.setLogin();
-        this.deleteData();
-        this.storeData();
-        this.password = "";
-      });
-    })
+    //if(this.status){
+      this.getAddress().subscribe((adresse : any) => {
+        this.adresse = adresse.trim();
+        this.getAuthentification().subscribe(auth => {
+          console.log(auth);
+          this.auth = auth;
+          this.message_connection = auth["message"];
+          if(auth["code"] == 200){
+            this.id_user = auth["id_user"];
+            this.access_level = auth["access_level"];
+            this.globalData.setIdUser(this.access_level);
+            this.globalData.setIpAddress(this.adresse);
+            this.globalData.setUserAccessLevel(this.access_level);
+            //this.router.navigate(['/login']);
+            this.router.navigate(['/home']);
+          }
+          //this.setLogin();
+          this.deleteData();
+          this.storeData();
+          this.password = "";
+        });
+      })
+    //}
   }
 
   /* public getLoginToFile() {
@@ -100,15 +128,15 @@ export class LoginPage implements OnInit {
   } */
 
   async storeData() {
-    await this.storage.set('login', this.login); // Stocker des données
+    await this.storage.set(this.public_login_store, this.login); // Stocker des données
   }
 
   async retrieveData() {
-    this.login = await this.storage.get('login'); // Récupérer des données
+    this.login = await this.storage.get(this.public_login_store) || ""; // Récupérer des données
   }
 
   async deleteData() {
-    await this.storage.remove('login'); // Supprimer des données
+    await this.storage.remove(this.public_login_store); // Supprimer des données
   }
 
   public deconnecterClick(){
@@ -125,5 +153,22 @@ export class LoginPage implements OnInit {
 
   ionViewWillEnter(){
     this.globalData.setShowMenu(this.show_menu);
+    this.retrieveData();
+    this.checkInternetConnection()
+  }
+
+  async checkInternetConnection() {
+    const status = await Network['getStatus']();
+    if (status.connected) {
+      //console.log('Connecté à Internet');
+      //this.status = true;
+      this.message_connection = "";
+      //this.presentAlert();
+    } else {
+      //console.log('Pas de connexion Internet');
+      //this.status = false;
+      this.message_connection = 'Pas de connexion Internet, veuillez activer votre wifi ou votre connexion de donnée';
+      this.presentAlert();
+    }
   }
 }
